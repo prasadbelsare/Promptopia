005c6961097a3eabc2796f9661172d683d8bef5b
Prasad Belsare <prasadbelsare64@gmail.com.com>
2025-03-02 17:56:07 -0700

    Fixed typo

--- app/api/auth/[...nextauth]/route.js
+++ app/api/auth/[...nextauth]/route.js
@@ -1,7 +1,7 @@
 import NextAuth from "next-auth";
 import GoogleProviders from "next-auth/providers/google";
 import { connectToDB } from "@utils/database";
-import User from "models/user";
+import User from "@models/user";
 
 const handler = NextAuth({
   providers: [
--- app/api/users/[id]/posts/route.js
+++ app/api/users/[id]/posts/route.js
@@ -1,5 +1,5 @@
 import { connectToDB } from "@utils/database";
-import Prompt from "models/prompt";
+import Prompt from "@models/prompt";
 
 export const GET = async (request, context) => {
   try {
